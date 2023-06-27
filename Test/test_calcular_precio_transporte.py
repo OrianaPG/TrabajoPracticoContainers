07313fc9c3db9daba5f94e603c326a192220554a
solGrana <116604252+solGrana@users.noreply.github.com>
2023-06-27 18:13:04 -0300

    Update test_calcular_precio_transporte.py

--- Test/test_calcular_precio_transporte.py
+++ Test/test_calcular_precio_transporte.py
@@ -5,31 +5,31 @@ from Container.ContenedorBasico import ContenedorBasico
 from Container.CalculadoraPrecio import calcular_costo_total
 from Carga import Carga
 
-def test_calcular_precio_transporte(TestCase):
+def test_calcular_precio_transporte():
     #retira container en el puerto
     distancia = 80 
     contenedor_completo = True
     carga = Carga(peso= 500, volumen=2.5)
     precio_esperado = 200000
-    assert calcular_costo_total(distancia, contenedor_completo, carga) == precio_esperado
+    assert calcular_costo_total(distancia, contenedor_completo, carga.__peso) == precio_esperado
 
     #puerta a puerta
     distancia = 1200 # menos de 10000
     contenedor_completo = False 
     carga = Carga(peso= 800, volumen=3.2)
     precio_esperado = 800 // 100 * 1150 #9200, cada 1150 cada 100kg
-    assert calcular_costo_total(distancia, contenedor_completo, carga) == precio_esperado
+    assert calcular_costo_total(distancia, contenedor_completo, carga.__peso) == precio_esperado
 
     # container completo
     distancia = 8500 
     contenedor_completo = True
     carga = Carga(peso= 2000, volumen=8.5)
     precio_esperado = 230000
-    assert calcular_costo_total(distancia, contenedor_completo, carga) == precio_esperado
+    assert calcular_costo_total(distancia, contenedor_completo, carga.__peso) == precio_esperado
 
     # usa prte del container
     distancia = 15000  
     contenedor_completo = False
     carga = Carga(peso= 500, volumen=2)
     precio_esperado = 500 // 100 * 1500 #7500, 1500 cda 100kg
-    assert calcular_costo_total(distancia, contenedor_completo, carga) == precio_esperado
+    assert calcular_costo_total(distancia, contenedor_completo, carga.__peso) == precio_esperado
