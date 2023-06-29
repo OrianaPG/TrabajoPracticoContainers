58c4e0a54bba4a78110c218876b19160b904e0ae
Sol Graña <116604252+solGrana@users.noreply.github.com>
2023-06-29 16:16:14 -0300

    agregue los metodos overideados

--- Clases/Container/ContenedorBasico.py
+++ Clases/Container/ContenedorBasico.py
@@ -6,3 +6,17 @@ class ContenedorBasico(Container):
         self.set_vol_maximo(32.6)
         self.set_peso_max(24000)
         self.set_pies(20)
+
+    def get_peso_max(self):
+        return self.__peso_max
+    
+    def puedeSubir(self, Carga):
+        #chequea si puede subir
+        if self.estaCompleto() or Carga.get_peso() > self.get_peso_max() or Carga.get_tipo() == "alimenticia": #falta chequear medidas
+            return False
+        
+    def cargarContainer(self, Carga):
+        #chequear si puede entrar la carga al contenedor
+        if self.puedeSubir(self, Carga):
+            #agregar la carga al container
+            self._completo = True
