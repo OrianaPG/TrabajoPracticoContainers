6f21ac1475d7ffd5c3c62a869148076712f7c1dc
Sol Graña <116604252+solGrana@users.noreply.github.com>
2023-06-29 17:35:45 -0300

    Update test_contenedoresyBarcos.py

--- Test/test_contenedoresyBarcos.py
+++ Test/test_contenedoresyBarcos.py
@@ -5,14 +5,28 @@ from Barcos import Barco
 
 class test_contenedoresyBarcos(unittest.TestCase):
     def test_encontrar_Container_mas_viajes_completo(self):
-        empresa = Empresa()  # instancia de la clase cmpresa con containers de prueba
+       empresa = Empresa()  # instancia de la clase cmpresa con containers de prueba
         # Agregar containeres de prueba a la empresa
         
-        Container1 = Container('001', True)
-        Container2 = Container('002', True)
-        Container3 = Container('003', False)
-        Container4 = Container('004', True)
-        Container5 = Container('005', True)
+        carga1 = Carga()
+        carga2 = Carga()
+        carga3 = Carga()
+        carga4 = Carga()
+        carga5 = Carga()
+
+        Container1 = Container('001')
+        Container2 = Container('002')
+        Container3 = Container('003')
+        Container4 = Container('004')
+        Container5 = Container('005')
+
+        barco1 = Barco('001', 5, 35000)
+        barco2 = Barco('002', 1, 100000)
+        barco3 = Barco('003', 10, 200000)
+
+        empresa.agregarBarco(barco1)
+        empresa.agregarBarco(barco2)
+        empresa.agregarBarco(barco3)
         
         empresa.agregarContenedor(Container1)
         empresa.agregarContenedor(Container2)
@@ -20,12 +34,28 @@ class test_contenedoresyBarcos(unittest.TestCase):
         empresa.agregarContenedor(Container4)
         empresa.agregarContenedor(Container5)
 
-        empresa.enviarContenedor(Container1)#falta aaclarar destino y origen
-        empresa.enviarContenedor(Container5)
-        empresa.enviarContenedor(Container3)
-        empresa.enviarContenedor(Container1)
-        empresa.enviarContenedor(Container2)
+        Container1.cargarContainer(carga1)
+        Container2.cargarContainer(carga2)
+        Container3.cargarContainer(carga3)
+        Container4.cargarContainer(carga4)
 
+        barco1.cargar_container(Container1)
+        barco2.cargar_container(Container3)
+        barco3.cargar_container(Container2)
+
+        empresa.enviar_barco(barco1)#falta aaclarar destino y origen
+        
+        Container1.cargarContainer(carga5)
+        
+        barco1.cargar_container(Container1)
+        
+        empresa.enviar_barco(barco1)
+        empresa.enviar_barco(barco2)
+        
+        barco2.cargar_container(Container4)
+    
+        empresa.enviar_barco(barco2)
+        empresa.enviar_barco(barco3)
         
         ContenedorMasViajesCompleto = empresa.encontrarContenedorMasViajesCompleto()
         
