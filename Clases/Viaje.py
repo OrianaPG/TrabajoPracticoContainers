20942fc3a4f5bd04c1259d24bab22b3c24d5719b
Sol Graña <116604252+solGrana@users.noreply.github.com>
2023-06-29 13:12:52 -0300

    cree clase Viaje para poder calcular lo de los containers

---
+++ Clases/Viaje.py
@@ -0,0 +1,10 @@
+from Container.Container import Container
+
+class Viaje:
+    def __init__(self, contenedor):
+        self.__contenedor = contenedor
+        self.__contenedorLleno = contenedor.estaCompleto()
+    def getContenedor(self):
+        return self.__contenedor
+    def getContenedorLleno(self):
+        return self.__contenedorLleno
