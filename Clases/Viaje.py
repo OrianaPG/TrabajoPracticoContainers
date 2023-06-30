9dbe925b7077fb759d95cfe1e6ce952aea87e976
Sol Graña <116604252+solGrana@users.noreply.github.com>
2023-06-29 21:12:31 -0300

    Update Viaje.py

--- Clases/Viaje.py
+++ Clases/Viaje.py
@@ -1,10 +1,14 @@
 from .Container.Container import Container
 
 class Viaje:
-    def __init__(self, contenedor):
+    def __init__(self, contenedor, distancia, puertaAPuerta):
         self.__contenedor = contenedor
         self.__contenedorLleno = contenedor.estaCompleto()
+        self.distancia = distancia
+        self.puertaAPuerta = puertaAPuerta
+
     def getContenedor(self):
         return self.__contenedor
+    
     def getContenedorLleno(self):
         return self.__contenedorLleno
